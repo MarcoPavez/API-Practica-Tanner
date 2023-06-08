d926a4c902bc6f35a28276977d5c5bb50916bf06
MarcoPavez <marcopz03@outlook.com>
2023-06-08 16:37:57 -0400

    Inicializacion del proyecto

---
+++ api-front/practica_asp.net/Models/CsharpApiContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace practica_asp.net.Models;
+
+public partial class CsharpApiContext : DbContext
+{
+    public CsharpApiContext()
+    {
+    }
+
+    public CsharpApiContext(DbContextOptions<CsharpApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categoria { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.IdCategoria).HasName("PK__categori__A3C02A10C8BBCA3A");
+
+            entity.ToTable("categoria");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("PK__producto__098892103529FDE7");
+
+            entity.ToTable("producto");
+
+            entity.Property(e => e.CodigoBarra)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Marca)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.IdCategoriaNavigation).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.IdCategoria)
+                .HasConstraintName("FK_IDCATEGORIA");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
